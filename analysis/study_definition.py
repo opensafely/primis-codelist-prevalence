e510aea1b541f9828b977a3fbd1a50cd75b2c045
Peter Inglesby <peter.inglesby@gmail.com>
2021-02-22 13:34:20 +0000

    Add codelists and study definition

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,168 +1,376 @@
+from datetime import date
 
-# Import functions
+from cohortextractor import StudyDefinition, patients
+import codelists
 
-from cohortextractor import (
-    StudyDefinition, 
-    patients, 
-    codelist, 
-    codelist_from_csv
-)
-
-# Import codelists
-
-chronic_cardiac_disease_codes = codelist_from_csv(
-    "codelists/opensafely-chronic-cardiac-disease.csv", system="ctv3", column="CTV3ID"
-)
-chronic_liver_disease_codes = codelist_from_csv(
-    "codelists/opensafely-chronic-liver-disease.csv", system="ctv3", column="CTV3ID"
-)
-salbutamol_codes = codelist_from_csv(
-    "codelists/opensafely-asthma-inhaler-salbutamol-medication.csv",
-    system="snomed",
-    column="id",
-)
-systolic_blood_pressure_codes = codelist(["2469."], system="ctv3")
-diastolic_blood_pressure_codes = codelist(["246A."], system="ctv3")
-
-
-# Specifiy study defeinition
 
 study = StudyDefinition(
-    # Configure the expectations framework
     default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
-        "rate": "exponential_increase",
+        "date": {"earliest": "1970-01-01", "latest": "today"},
+        "rate": "uniform",
+        "incidence": 0.1,
     },
-    # This line defines the study population
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
-    ),
 
-    # https://github.com/opensafely/risk-factors-research/issues/49
+    population=patients.all(),
+
     age=patients.age_as_of(
-        "2020-02-01",
+        "2021-01-01",
         return_expectations={
-            "rate": "universal",
-            "int": {"distribution": "population_ages"},
+            "int": {'distribution': 'normal', 'mean': 40, 'stddev': 20},
         },
     ),
-    
-    # https://github.com/opensafely/risk-factors-research/issues/46
+
     sex=patients.sex(
         return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"M": 0.49, "F": 0.51}},
-        }
+            "category": {'ratios': {'F': 0.495, 'M': 0.495, '': 0.01}},
+        },
     ),
-    
-    # https://codelists.opensafely.org/codelist/opensafely/chronic-cardiac-disease/2020-04-08/
-    chronic_cardiac_disease=patients.with_these_clinical_events(
-        chronic_cardiac_disease_codes,
+
+    # Asthma Diagnosis code
+    ast=patients.with_these_clinical_events(
+        codelists.ast,
         returning="date",
-        find_first_match_in_period=True,
-        include_month=True,
-        return_expectations={"incidence": 0.2},
+        date_format="YYYY",
+        find_last_match_in_period=True,
     ),
 
-    # https://codelists.opensafely.org/codelist/opensafely/chronic-liver-disease/2020-06-02/
-    chronic_liver_disease=patients.with_these_clinical_events(
-        chronic_liver_disease_codes,
+    # Asthma Admission codes
+    astadm=patients.with_these_clinical_events(
+        codelists.astadm,
         returning="date",
-        find_first_match_in_period=True,
-        include_month=True,
-        return_expectations={
-            "incidence": 0.2,
-            "date": {"earliest": "1950-01-01", "latest": "today"},
-        },
+        date_format="YYYY",
+        find_last_match_in_period=True,
     ),
 
-    # https://github.com/opensafely/risk-factors-research/issues/51
-    bmi=patients.most_recent_bmi(
-        on_or_after="2010-02-01",
-        minimum_age_at_measurement=16,
-        include_measurement_date=True,
-        include_month=True,
-        return_expectations={
-            "incidence": 0.6,
-            "float": {"distribution": "normal", "mean": 35, "stddev": 10},
-        },
+    # Asthma systemic steroid prescription codes
+    astrx=patients.with_these_medications(
+        codelists.astrx,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
     ),
 
-    # https://github.com/opensafely/risk-factors-research/issues/48
-    bp_sys=patients.mean_recorded_value(
-        systolic_blood_pressure_codes,
-        on_most_recent_day_of_measurement=True,
-        on_or_before="2020-02-01",
-        include_measurement_date=True,
-        include_month=True,
-        return_expectations={
-            "incidence": 0.6,
-            "float": {"distribution": "normal", "mean": 80, "stddev": 10},
-        },
-    ),
-    
-    # https://github.com/opensafely/risk-factors-research/issues/48
-    bp_dias=patients.mean_recorded_value(
-        diastolic_blood_pressure_codes,
-        on_most_recent_day_of_measurement=True,
-        on_or_before="2020-02-01",
-        include_measurement_date=True,
-        include_month=True,
-        return_expectations={
-            "incidence": 0.6,
-            "float": {"distribution": "normal", "mean": 120, "stddev": 10},
-        },
+    # BMI
+    bmi=patients.with_these_clinical_events(
+        codelists.bmi,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
     ),
 
-    # https://github.com/opensafely/risk-factors-research/issues/44
-    stp=patients.registered_practice_as_of(
-        "2020-02-01",
-        returning="stp_code",
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"STP1": 0.5, "STP2": 0.5}},
-        },
-    ),
-    
-    # https://github.com/opensafely/risk-factors-research/issues/44
-    msoa=patients.registered_practice_as_of(
-        "2020-02-01",
-        returning="msoa_code",
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"MSOA1": 0.5, "MSOA2": 0.5}},
-        },
+    # All BMI coded terms
+    bmi_stage=patients.with_these_clinical_events(
+        codelists.bmi_stage,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
     ),
 
-    # https://github.com/opensafely/risk-factors-research/issues/45
-    imd=patients.address_as_of(
-        "2020-02-01",
-        returning="index_of_multiple_deprivation",
-        round_to_nearest=100,
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"100": 0.1, "200": 0.2, "300": 0.7}},
-        },
+    # Employed by Care Home codes
+    carehome=patients.with_these_clinical_events(
+        codelists.carehome,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
     ),
 
-    # https://github.com/opensafely/risk-factors-research/issues/47
-    rural_urban=patients.address_as_of(
-        "2020-02-01",
-        returning="rural_urban_classification",
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"rural": 0.1, "urban": 0.9}},
-        },
+    # Carer codes
+    carer=patients.with_these_clinical_events(
+        codelists.carer,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
     ),
- 
-    # https://codelists.opensafely.org/codelist/opensafely/asthma-inhaler-salbutamol-medication/2020-04-15/
-    recent_salbutamol_count=patients.with_these_medications(
-        salbutamol_codes,
-        between=["2018-02-01", "2020-02-01"],
-        returning="number_of_matches_in_period",
-        return_expectations={
-            "incidence": 0.6,
-            "int": {"distribution": "normal", "mean": 8, "stddev": 2},
+
+    # Chronic heart disease codes
+    chd_cov=patients.with_these_clinical_events(
+        codelists.chd_cov,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Chronic kidney disease codes - all stages
+    ckd15=patients.with_these_clinical_events(
+        codelists.ckd15,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Chronic kidney disease codes-stages 3 - 5
+    ckd35=patients.with_these_clinical_events(
+        codelists.ckd35,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Chronic kidney disease diagnostic codes
+    ckd_cov=patients.with_these_clinical_events(
+        codelists.ckd_cov,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Chronic Liver disease codes
+    cld=patients.with_these_clinical_events(
+        codelists.cld,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Chronic Neurological Disease including Significant Learning Disorder
+    cns_cov=patients.with_these_clinical_events(
+        codelists.cns_cov,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # First COVID vaccination declined
+    cov1decl=patients.with_these_clinical_events(
+        codelists.cov1decl,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Second COVID vaccination declined
+    cov2decl=patients.with_these_clinical_events(
+        codelists.cov2decl,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # First COVID vaccination administration codes
+    covadm1=patients.with_these_clinical_events(
+        codelists.covadm1,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Second COVID vaccination administration codes
+    covadm2=patients.with_these_clinical_events(
+        codelists.covadm2,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # COVID vaccination contraindication codes
+    covcontra=patients.with_these_clinical_events(
+        codelists.covcontra,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    covrx=patients.with_vaccination_record(
+        tpp={
+            "product_name_matches": ["COVID-19 mRNA Vac BNT162b2 30mcg/0.3ml conc for susp for inj multidose vials (Pfizer-BioNTech)", "COVID-19 Vac AstraZeneca (ChAdOx1 S recomb) 5x10000000000 viral particles/0.5ml dose sol for inj MDV", "TODO", "TODO"],
         },
+        emis={
+            "product_codes": codelists.covrx,
+        },
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Diabetes diagnosis codes
+    diab=patients.with_these_clinical_events(
+        codelists.diab,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Diabetes resolved codes
+    dmres=patients.with_these_clinical_events(
+        codelists.dmres,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Employed by domiciliary care provider codes
+    domcare=patients.with_these_clinical_events(
+        codelists.domcare,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Ethnicity codes
+    eth2001=patients.with_these_clinical_events(
+        codelists.eth2001,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Ethnicity no record
+    eth_norecord=patients.with_these_clinical_events(
+        codelists.eth_norecord,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Ethnicity not given - patient refused
+    eth_notgiptref=patients.with_these_clinical_events(
+        codelists.eth_notgiptref,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Ethnicity not stated
+    eth_notstated=patients.with_these_clinical_events(
+        codelists.eth_notstated,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # to represent household contact of shielding individual
+    hhld_imdef=patients.with_these_clinical_events(
+        codelists.hhld_imdef,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Immunosuppression diagnosis codes
+    immdx_cov=patients.with_these_clinical_events(
+        codelists.immdx_cov,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Immunosuppression medication codes
+    immrx=patients.with_these_medications(
+        codelists.immrx,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Wider Learning Disability
+    learndis=patients.with_these_clinical_events(
+        codelists.learndis,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Patients in long-stay nursing and residential care
+    longres=patients.with_these_clinical_events(
+        codelists.longres,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Any other ethnicity code
+    non_eth2001=patients.with_these_clinical_events(
+        codelists.non_eth2001,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Lower Risk from COVID-19 codes
+    nonshield=patients.with_these_clinical_events(
+        codelists.nonshield,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # No longer a carer codes
+    notcarer=patients.with_these_clinical_events(
+        codelists.notcarer,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Employed by nursing home codes
+    nursehome=patients.with_these_clinical_events(
+        codelists.nursehome,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Pregnancy codes recorded in the 8.5 months before the audit run date
+    preg=patients.with_these_clinical_events(
+        codelists.preg,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Pregnancy or Delivery codes recorded in the 8.5 months before audit run date
+    pregdel=patients.with_these_clinical_events(
+        codelists.pregdel,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Chronic Respiratory Disease
+    resp_cov=patients.with_these_clinical_events(
+        codelists.resp_cov,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Severe Mental Illness codes
+    sev_mental=patients.with_these_clinical_events(
+        codelists.sev_mental,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Severe Obesity code recorded
+    sev_obesity=patients.with_these_clinical_events(
+        codelists.sev_obesity,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # High Risk from COVID-19 code
+    shield=patients.with_these_clinical_events(
+        codelists.shield,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Remission codes relating to Severe Mental Illness
+    smhres=patients.with_these_clinical_events(
+        codelists.smhres,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),
+
+    # Asplenia or Dysfunction of the Spleen codes
+    spln_cov=patients.with_these_clinical_events(
+        codelists.spln_cov,
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
     ),
 )
---
+++ generate_code.py
@@ -0,0 +1,101 @@
+import re
+import requests
+
+
+rsp = requests.get(
+    "https://codelists.opensafely.org/api/v1/codelist/primis-covid19-vacc-uptake/?format=json"
+)
+
+
+records = sorted(rsp.json(), key=lambda r: r["slug"])
+
+with open("codelists/codelists.txt", "w") as f:
+    for r in records:
+        f.write(r["versions"][-1]["full_slug"] + "\n")
+
+
+with open("analysis/codelists.py", "w") as f:
+    f.write("from cohortextractor import codelist_from_csv\n\n")
+
+    for r in records:
+        f.write("\n")
+        f.write(f"# {r['name']}\n")
+        f.write(f"{r['slug']} = codelist_from_csv(\n")
+        f.write(f'    "codelists/primis-covid19-vacc-uptake-{r["slug"]}.csv",\n')
+        f.write(f'    system="snomed",\n')
+        f.write('    column="code",\n')
+        f.write(")\n")
+
+
+with open("analysis/study_definition.py", "w") as f:
+    f.write(
+        """
+from datetime import date
+
+from cohortextractor import StudyDefinition, patients
+import codelists
+
+
+study = StudyDefinition(
+    default_expectations={
+        "date": {"earliest": "1970-01-01", "latest": "today"},
+        "rate": "uniform",
+        "incidence": 0.1,
+    },
+
+    population=patients.all(),
+
+    age=patients.age_as_of(
+        "2021-01-01",
+        return_expectations={
+            "int": {'distribution': 'normal', 'mean': 40, 'stddev': 20},
+        },
+    ),
+
+    sex=patients.sex(
+        return_expectations={
+            "category": {'ratios': {'F': 0.495, 'M': 0.495, '': 0.01}},
+        },
+    ),
+    """.strip()
+        + "\n"
+    )
+
+    for r in records:
+        if "drx" in r["slug"] or "eth2001_" in r["slug"]:
+            # Skip these because they're subsets of other codelists
+            continue
+
+        if r["slug"] == "covrx":
+            f.write(
+                """
+    covrx=patients.with_vaccination_record(
+        tpp={
+            "product_name_matches": ["COVID-19 mRNA Vac BNT162b2 30mcg/0.3ml conc for susp for inj multidose vials (Pfizer-BioNTech)", "COVID-19 Vac AstraZeneca (ChAdOx1 S recomb) 5x10000000000 viral particles/0.5ml dose sol for inj MDV", "TODO", "TODO"],
+        },
+        emis={
+            "product_codes": codelists.covrx,
+        },
+        returning="date",
+        date_format="YYYY",
+        find_last_match_in_period=True,
+    ),\n"""
+            )
+
+        else:
+            fn = (
+                "with_these_medications"
+                if "rx" in r["slug"]
+                else "with_these_clinical_events"
+            )
+
+            f.write("\n")
+            f.write(f"    # {r['name']}\n")
+            f.write(f"    {r['slug']}=patients.{fn}(\n")
+            f.write(f"        codelists.{r['slug']},\n")
+            f.write(f'        returning="date",\n')
+            f.write(f'        date_format="YYYY",\n')
+            f.write(f"        find_last_match_in_period=True,\n")
+            f.write("    ),\n")
+
+    f.write(")")
